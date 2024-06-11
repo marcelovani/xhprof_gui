1502d0fcacfd8badef8fbc77950659d67f6e3697
Marcelo Vani <marcellovani@yahoo.co.uk>
2024-06-11 12:20:40 +0100

    Porting changes to Gui.

--- xhprof_lib/defaults.php
+++ xhprof_lib/defaults.php
@@ -1,9 +1,10 @@
 <?php
+$XHPROF_ROOT = realpath(dirname(__FILE__) .'/..');
 
 if (!defined('XHPROF_LIB_ROOT')) {
   define('XHPROF_LIB_ROOT', dirname(__FILE__));
 }
 
 if (!defined('XHPROF_CONFIG')) {
-  define('XHPROF_CONFIG', XHPROF_LIB_ROOT . '/config.php');
+  define('XHPROF_CONFIG', $XHPROF_ROOT . '/config.php');
 }
