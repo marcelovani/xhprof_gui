a80112d4f34fe05c4ae47f6dcb99e64efc9430d9
Marcelo Vani <marcellovani@yahoo.co.uk>
2024-06-11 15:27:07 +0100

    Added callback apis.

---
+++ xhprof_html/api/db/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once getcwd() . '/../../../xhprof_lib/defaults.php';
+require_once XHPROF_CONFIG;
+require_once XHPROF_LIB_ROOT . '/params.php';
+
+if (false !== $controlIPs && !in_array($_SERVER['REMOTE_ADDR'], $controlIPs))
+{
+  die("You do not have permission to view this page.");
+}
+
+include_once XHPROF_LIB_ROOT . '/display/xhprof.php';
+
+ini_set('max_execution_time', 100);
+
+$xhprof_runs_impl = new XHProfRuns_Default();
+
+if (!empty($run)) {
+  $digraph = xhprof_render_dot($xhprof_runs_impl, $run, $type, $threshold, $func, $source, $critical, $show_internal, $links);
+
+  print_r($digraph);
+}
+else {
+  die('Something went wrong.');
+}
---
+++ xhprof_html/api/file/index.php
@@ -0,0 +1,34 @@
+<?php
+require_once getcwd() . '/../../../xhprof_lib/defaults.php';
+require_once XHPROF_CONFIG;
+require_once XHPROF_LIB_ROOT . '/params.php';
+
+if (false !== $controlIPs && !in_array($_SERVER['REMOTE_ADDR'], $controlIPs))
+{
+  die("You do not have permission to view this page.");
+}
+
+include_once XHPROF_LIB_ROOT . '/display/xhprof.php';
+
+ini_set('max_execution_time', 100);
+
+if (!empty($run)) {
+  $filename = '/traces/' . $run . '.xhprof';
+  $contents = file_get_contents($filename);
+  $raw_data = unserialize($contents);
+  $contents = null;
+  $raw_data = (isset($raw_data['data'])) ? $raw_data['data'] : $raw_data;
+  $source = 'xhprof';
+  $page = 'XHProf Run (Namespace=xhprof)';
+  $critical_path = true;
+  $right = null;
+  $left = null;
+
+  $digraph = xhprof_generate_dot_script($raw_data, $threshold, $source, $page, $func, $critical_path, $right, $left, $show_internal, $links);
+  $raw_data = null;
+
+  print_r($digraph);
+}
+else {
+  die('Something went wrong.');
+}
