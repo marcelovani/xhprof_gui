0b6b82925a9cc079246215565b274c8e3f08da12
Marcelo Vani <marcellovani@yahoo.co.uk>
2024-06-11 15:27:53 +0100

    Moving code.

---
+++ xhprof_lib/templates/graph_filter_options.php
@@ -0,0 +1,53 @@
+<?php require_once getcwd() . '/../../xhprof_lib/utils/common.php'; ?>
+<div id="options">
+<?php echo get_home_button(); ?>
+  <label id="engine">
+    Engine:
+    <select>
+      <option>circo</option>
+      <option selected>dot</option>
+      <option>fdp</option>
+      <option>neato</option>
+      <option>osage</option>
+      <option>twopi</option>
+    </select>
+  </label>
+
+  <label id="format">
+    Format:
+    <select>
+      <option selected>svg</option>
+      <option>png</option>
+      <option>xdot</option>
+      <option>plain</option>
+      <option>ps</option>
+      <option>3D</option>
+    </select>
+  </label>
+
+  <label id="raw">
+    <input type="checkbox"> Show raw output
+  </label>
+
+  <?php echo get_show_internal_button('Show internal functions', 1); ?>
+  <span class="threshold">Threshold
+    <?php
+    echo get_threshold_button('++', 0.1, $threshold);
+    echo get_threshold_button('+', 0.01, $threshold);
+    echo get_threshold_button('-', -0.01, $threshold);
+    echo get_threshold_button('--', -0.1, $threshold);
+    ?>
+  </span>
+  <span id="resetCamera">
+    <a onclick="resetCamera()">Reset camera</a>
+  </span>
+  <span id="debug">
+    <span class="a"></span>
+    <span class="b"></span>
+    <span class="c"></span>
+    <span class="d"></span>
+    <span class="e"></span>
+    <span class="f"></span>
+  </span>
+
+</div>
